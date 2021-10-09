006f502a5e0f9167a3c28a1251bcbb5db2faaf77
knockcat <85362504+knockcat@users.noreply.github.com>
2021-10-09 18:43:45 +0530

    Add files via upload

---
+++ Looping Programs/For_input_5_numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+ {
+     int a;
+     printf("ENTER 5 numbers=>\n");
+     for(a=0;a<=5;a++)
+     {
+        scanf("%d",&a);
+     printf("a is=>%d\n",a);
+     }
+     return 0;
+ }

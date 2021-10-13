8c3b2146be67133b095cc126727fb3a6ab98edbd
knockcat <85362504+knockcat@users.noreply.github.com>
2021-10-13 21:54:14 +0530

    Add files via upload

---
+++ DataTypes/screw4.cpp
@@ -0,0 +1,18 @@
+#include<Stdio.h>
+//Data Type Conversion
+// One more Example Type Casting
+// EXAMPLE 6
+//NOTE: INCLUDE THE HEADER FILE stdio.h
+int main()
+{
+    short int sh ;  
+    int it ; float ft  = 8.9 ;  
+
+    sh = it = ( ft = ft / 3) * 3 ;
+
+    printf ("short = %d,int = %d ,float = %6.2f \n", sh, it, ft);
+
+    return 0;
+}
+/* short = 8,int = 8 ,float =   2.97  */
+

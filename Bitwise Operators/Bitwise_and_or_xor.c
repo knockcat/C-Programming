cb1241010364bd2cdc5878b0bb67aa897687b966
knockcat <85362504+knockcat@users.noreply.github.com>
2021-10-20 00:23:18 +0530

    Add files via upload

---
+++ Bitwise Operators/Bitwise_and_or_xor.c
@@ -0,0 +1,17 @@
+//bit-wise operator
+
+#include<stdio.h>
+#define  A 0 
+#define B 1
+int main()
+{
+	printf("%d & %d = %d",A,B,A&B);
+	
+	printf("\n%d | %d = %d",A,B,A|B);
+	
+	printf("\n%d ^ %d = %d",A,B,A^B);
+	
+	return 0;
+	
+}
+

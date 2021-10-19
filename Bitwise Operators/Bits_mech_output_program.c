4591eaf2d9fed1527854bdc85b330c6993bede1e
knockcat <85362504+knockcat@users.noreply.github.com>
2021-10-20 00:23:08 +0530

    Add files via upload

---
+++ Bitwise Operators/Bits_mech_output_program.c
@@ -0,0 +1,15 @@
+//bits mechanism
+#include<stdio.h>
+void bin(unsigned n)
+{
+    unsigned i;
+    for (i = 1 << 31; i > 0; i = i / 2)
+        (n & i) ? printf("1") : printf("0");
+}
+ 
+int main(void)
+{
+    bin(7);
+    printf("\n");
+    bin(4);
+}

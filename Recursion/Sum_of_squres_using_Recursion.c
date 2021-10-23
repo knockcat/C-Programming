d73878fb1e7f7b7a90ba7deff2bc73c158842f86
knockcat <85362504+knockcat@users.noreply.github.com>
2021-10-23 18:24:44 +0530

    Add files via upload

---
+++ Recursion/Sum_of_squres_using_Recursion.c
@@ -0,0 +1,29 @@
+//sum of series 1^! - 2^2 + 3^3 - ..................................................................................
+
+#include<stdio.h>
+int s(int n)
+{
+    static int i=1,sign=1,sum=0,sum1=0,Tsum=0;
+    if(i<=n)
+    {
+        if(i%2==0)
+        {
+            sum+=i*i;
+        }
+        else
+        {
+            sum1+=i*i;
+        }
+        i++;
+        s(n);
+    }
+    Tsum=sum1-sum;
+    return Tsum;
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    printf("%d",s(n));
+    return 0;
+}
